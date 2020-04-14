625476dc9bdc326c8313ea838fd97e12a2b1cb6e
Fredson Chaves <fredsonchaves07@gmail.com>
2020-04-14 14:02:18 -0300

    Estruturação das pastas public e view

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.set('view engine', 'njk')
+nunjucks.configure('views', {
+    express: server
+})
+server.use(express.static('public'))
+
+server.get('/', function(req, res){
+    return res.render('layout')
+})
 server.listen(5000, function(){
     console.log('Server is running..')
 })
